5a5b3ef7519feca5d12bc00553bbdc094b2acf8b
tomw <6361pico>
2017-02-12 21:26:00 +0000

    Adding basic implementation to update html table

---
+++ src/main/java/com/twillmott/traktbrowser/model/Series.java
@@ -0,0 +1,44 @@
+package com.twillmott.traktbrowser.model;
+
+/**
+ * Created by tomw on 12/02/2017.
+ */
+public class Series {
+
+    private String name;
+    private int seasons;
+    private boolean continuing;
+    private boolean watched;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public boolean isContinuing() {
+        return continuing;
+    }
+
+    public void setContinuing(boolean continuing) {
+        this.continuing = continuing;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+}
