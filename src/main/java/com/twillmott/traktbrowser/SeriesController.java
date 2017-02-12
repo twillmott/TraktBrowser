da147067a8e5696cf7b56a56cb6bf065b843bafb
Tom Willmott <tom@willmott.org.uk>
2017-02-12 21:26:00 +0000

    Adding basic implementation to update html table

--- src/main/java/com/twillmott/traktbrowser/SeriesController.java
+++ src/main/java/com/twillmott/traktbrowser/SeriesController.java
@@ -1,11 +1,14 @@
 package com.twillmott.traktbrowser;
 
+import com.google.common.collect.Lists;
+import com.twillmott.traktbrowser.model.Series;
 import com.twillmott.traktbrowser.service.FileScanner;
 import com.twillmott.traktbrowser.service.TraktService;
 import com.uwetrottmann.trakt5.entities.BaseShow;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -18,10 +21,27 @@ public class SeriesController {
 
     TraktService traktService = new TraktService();
 
-    @RequestMapping("/tvshows")
+    @RequestMapping(value = "/tvshows", method = RequestMethod.GET)
     public String greeting(Model model) {
-        FileScanner fs = new FileScanner();
-        fs.getTvShows();
-        return null;
+//        FileScanner fs = new FileScanner();
+//        fs.getTvShows();
+
+        Series series1 = new Series();
+        series1.setName("Breaking Bad");
+        series1.setSeasons(10);
+        series1.setContinuing(true);
+        series1.setWatched(false);
+
+        Series series2 = new Series();
+        series2.setName("New Girl");
+        series2.setSeasons(12);
+        series2.setContinuing(false);
+        series2.setWatched(true);
+
+        List<Series> series = Lists.newArrayList(series1, series2);
+
+        model.addAttribute("series", series);
+
+        return "tvshows";
     }
 }
